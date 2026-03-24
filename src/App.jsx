9f709caedff9b1f1721250ea9e49810bfe1dfe1f
Sanjaypandi123 <sanjayelanchikani@gmail.com>
2026-03-24 12:16:13 +0530

    total-completed

--- src/App.jsx
+++ src/App.jsx
@@ -25,15 +25,8 @@ import swal from 'sweetalert2';
 
 function App() {
 
-  const [moods, setMoods] = useState([]);
+  const [moods, setMoods] = useState(JSON.parse(localStorage.getItem("moods")) ||[]);
   const [open, setOpen] = useState(false)
-
-
-  useEffect(() => {
-    const data = JSON.parse(localStorage.getItem("moods")) || [];
-    setMoods(data);
-  }, []);
-
 
   useEffect(() => {
     localStorage.setItem("moods", JSON.stringify(moods)) || [];
@@ -53,7 +46,8 @@ function App() {
         confirmButtonText: "OK"
       });
       return;
-    } else {
+    }
+    else {
       swal.fire({
         title: "Success!",
         text: "Your Emotion was Added successfully",
@@ -92,76 +86,86 @@ function App() {
   const analyze = () => {
     if (moods.length === 0) return "";
 
-    const avg =
-      moods.reduce((sum, m) => sum + m.mood, 0) / moods.length;
-
+    const avg = moods.reduce((sum, m) => sum + m.mood, 0) / moods.length;
+    if (moods.length === 7) {
     if (avg >= 75) return "😍 Happy Week";
     if (avg >= 50) return "😌 Normal Week";
     return "😡 Bad Week";
-  };
+  }
+  if(moods.length > 1){
+    if (avg >= 75) return "😍 Happy days";
+  if (avg >= 50) return "😌 Normal days";
+  return "😡 Bad days";
+  }
+  if(moods.length === 1){
+    if (avg >= 75) return "😍 Happy day";
+  if (avg >= 50) return "😌 Normal day";
+  return "😡 Bad day";
+  }
+};
 
 
 
 
 
-  return (
-    <>
-      <div className="container">
-        <div className="wrapper">
-          <div className="header">
-            <h1>Emotion Tracker</h1>
-            <p>Track your daily mood & analyze your week</p>
-          </div>
-
-
-          <div className="emoji-section">
-
-            <button onClick={() => addMood(100)}><First /></button>
-            <button onClick={() => addMood(75)}><Second /></button>
-            <button onClick={() => addMood(50)}><Thirdemo /></button>
-            <button onClick={() => addMood(25)}><Fourthemo /></button>
-            <button onClick={() => addMood(0)}><Fifthemo /></button>
-
-
-
-          </div>
-          <div className="BTN">
-
-            <div className="action">
-              {/* {moods.length===7 ? <button className="view-btn" onClick={() => HANDLEOPEN()}>View Emotions</button> : ""} */}
-              <button className="view-btn" onClick={() => HANDLEOPEN()}>View Emotions</button>
-            </div>
-          </div>
+return (
+  <>
+    <div className="container">
+      <div className="wrapper">
+        <div className="header">
+          <h1>Emotion Tracker</h1>
+          <p>Track your daily mood & analyze your week</p>
         </div>
 
 
+        <div className="emoji-section">
+
+          <button onClick={() => addMood(100)}><First /></button>
+          <button onClick={() => addMood(75)}><Second /></button>
+          <button onClick={() => addMood(50)}><Thirdemo /></button>
+          <button onClick={() => addMood(25)}><Fourthemo /></button>
+          <button onClick={() => addMood(0)}><Fifthemo /></button>
 
 
 
-        {open && (<div className="popup-overlay">
+        </div>
+        <div className="BTN">
 
-          <div className="popup-box">
-
-            <button className="close-btn" onClick={() => setOpen(false)}>X</button>
-
-            <h2>Your Last 7 Days Emotions</h2>
-
-
-
-            <div className="crt" >
-              <Line data={chartData} />
-            </div>
-
-
-            <h2>{analyze()}</h2>
-
+          <div className="action">
+            {/* {moods.length===7 ? <button className="view-btn" onClick={() => HANDLEOPEN()}>View Emotions</button> : ""} */}
+            <button className="view-btn" onClick={() => HANDLEOPEN()}>View Emotions</button>
           </div>
-
-        </div>)}
+        </div>
       </div>
 
-    </>
-  );
+
+
+
+
+      {open && (<div className="popup-overlay">
+
+        <div className="popup-box">
+
+          <button className="close-btn" onClick={() => setOpen(false)}>X</button>
+
+          <h2>Your Last 7 Days Emotions</h2>
+
+
+
+          <div className="crt" >
+            <Line data={chartData} />
+          </div>
+
+
+          <h2>{analyze()}</h2>
+
+        </div>
+
+      </div>)}
+    </div>
+
+  </>
+);
 }
 
 export default App
